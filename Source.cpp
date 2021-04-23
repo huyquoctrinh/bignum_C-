af6aae620bdd48a07fa140ad0a06f6e9ae6fc1e4
unknown <trnhquchuy@yahoo.com.vn>
2021-04-23 22:11:30 +0700

    initial

---
+++ Source.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<string>
+#define MAX 1024
+using namespace std;
+struct bigint {
+	string data;
+};
+string bigint2string(bigint a) {
+	char* x = new char[MAX];
+	return x;
+}
+bigint string2bigint(string a) {
+	bigint res;
+	res.data = a;
+	return res;
+}
+string value2string(int a) {
+	string p = to_string(a);
+	return p;
+}
+int getLength(bigint a) {
+	return a.data.length();
+}
+int getDigit(bigint a,int pos) {
+	int n = getLength(a);
+	for (int i = 0; i < n; i++) {
+		if (pos < n) {
+			return a.data[pos] - 48;
+		}
+	}
+	return 0;
+}
